1b775c58a1c77d2919fddf164c9a3564bb66cd41
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-11-20 17:13:48 -0600

    working on doodl project

---
+++ Components/Doodl.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+export default function Doodl() {
+  <div href="doodlit.me" className={styles.doodl}>
+    <img src="/doodl.png" className={styles.doodlImg} />
+    <div className={styles.descCon}>
+      <p className={styles.descTitle}>Fun Drawing Game</p>
+      <p className={styles.descPro}>Personal Project</p>
+    </div>
+  </div>;
+}

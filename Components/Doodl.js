218b0eb7c6930c149723f7ec06c7860a6f6d11a2
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-11-20 23:19:19 -0600

    finished doodl component

--- Components/Doodl.js
+++ Components/Doodl.js
@@ -1,10 +1,14 @@
 import styles from "../styles/Home.module.css";
 export default function Doodl() {
-  <div href="doodlit.me" className={styles.doodl}>
-    <img src="/doodl.png" className={styles.doodlImg} />
-    <div className={styles.descCon}>
-      <p className={styles.descTitle}>Fun Drawing Game</p>
-      <p className={styles.descPro}>Personal Project</p>
-    </div>
-  </div>;
+  return (
+    <a href="http://doodlit.me">
+      <div className={styles.des}>
+        <img src="/doodl.png" className={styles.desImg} />
+        <div className={styles.desCon}>
+          <p className={styles.desTitle}>Fun Drawing Game</p>
+          <p className={styles.desPro}>Personal Project</p>
+        </div>
+      </div>
+    </a>
+  );
 }
--- Components/Projects.js
+++ Components/Projects.js
@@ -1,9 +1,12 @@
 import styles from "../styles/Home.module.css";
+import Doodl from "./Doodl.js";
 export default function Projects() {
   return (
     <div className={styles.projects} id="Projects">
       <h2 className={styles.projectsTitle}>Projects:</h2>
-      <div className={styles.projectsContainer}></div>
+      <div className={styles.projectsContainer}>
+        <Doodl />
+      </div>
     </div>
   );
 }

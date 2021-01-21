9a329a2f44b762d5d45bad7b26e3d27fc628202b
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-01-21 16:29:23 -0600

    fixed media query bug

--- Components/Intro.js
+++ Components/Intro.js
@@ -3,7 +3,11 @@ export default function Intro() {
   return (
     <div className={styles.title}>
       <div className={styles.intro}>
-        <h1 className={styles.introName}>Masen Funderburk</h1>
+        <h1 className={styles.introName}>
+          Masen
+          <br />
+          Funderburk
+        </h1>
         <p className={styles.introTitle}>Full Stack Web Developer</p>
       </div>
       <img className={styles.introImg} src="/Masen.png" />
